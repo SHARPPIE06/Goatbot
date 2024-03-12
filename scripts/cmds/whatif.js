f5a6d7498a476e5c2106241f9b6b7602aaf5155f
SHARPPIE06 <152580850+SHARPPIE06@users.noreply.github.com>
2024-03-12 20:32:44 +0800

    Update whatif.js

--- scripts/cmds/whatif.js
+++ scripts/cmds/whatif.js
@@ -1,36 +1,36 @@
 module.exports = {
     config: {
-        name: 'love',
-        author: 'HASSAN-',
+        name: 'whatif',
+        author: 'Harley',
         version: 1.0,
         role: 0,
         shortDescription: {
-            en: 'Spread love with beautiful messages.'
+            en: 'what if.'
         },
         longDescription: {
-            en: 'Sends lovely messages to brighten someone\s day.'
+            en: 'Sends random "what if" messages.'
         },
         category: 'fun',
         guide: {
-            en: 'Simply use the command to send a message of love.'
+            en: 'Simply use the command to send a random "what if" message.'
         }
     },
     event: null,
     onStart: async function({ api, event, args }) {
-        const loveMessages = [
-            'You are loved more than you know. 💖',
-            "You're amazing and deserving of all the love and happiness in the world. Here's some virtual love to brighten your day! 🤩💖",
-            "I'm sending a virtual hug and a big dose of positive energy to keep shining your light! 😘💫",
-            "I'm here for you and sending love and support. Virtual hugs and snuggles! 🤩💕",
-            'I am worthy of love and respect regardless of my flaws or mistakes.',
-            'I am deserving of happiness and success, and I am worthy of my dreams. 🌌',
-            'I am strong and resilient, and I can overcome any obstacles that come my way.',
-'Hello baby, come to papa Harley 🥵',
-'Hello love, single si Boss Harley pm mo na sya'
+        const whatIfMessages = [
+            'What if we could fly without wings?',
+            'What if we could travel through time?',
+            'What if we could communicate with animals?',
+            'What if we could live underwater?',
+            'What if we could teleport instantly?',
+            'What if we could read minds?',
+            'What if we could speak every language?',
+            'What if we could control the weather?',
+            // Add more "what if" messages here
         ];
 
-        const randomLoveMessage = loveMessages[Math.floor(Math.random() * loveMessages.length)];
+        const randomWhatIfMessage = whatIfMessages[Math.floor(Math.random() * whatIfMessages.length)];
 
-        api.sendMessage(randomLoveMessage, event.threadID);
+        api.sendMessage(randomWhatIfMessage, event.threadID);
     }
 };
